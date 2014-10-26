d6de10c5f6579a78695602a346e352a35a520b49
Patrick Kromeyer <patrick.kromeyer@gmail.com>
2014-10-26 12:17:10 +0100

    added export

--- js/src/timeTracker/controller.js
+++ js/src/timeTracker/controller.js
@@ -3,10 +3,11 @@
 
     angular.module('timeTracker').controller('timeTrackerController', [
         '$scope',
+        '$window',
         '$interval',
         'timeTrackerEntryRepositoryService',
         'timeTrackerCalculationService',
-        function ($scope, $interval, timeTrackerEntryRepository, timeTrackerCalculation) {
+        function ($scope, $window, $interval, timeTrackerEntryRepository, timeTrackerCalculation) {
 
             $scope.timeTrackerEntries = timeTrackerEntryRepository.fetchEntries();
 
@@ -18,6 +19,25 @@
             $scope.reset = function () {
                 timeTrackerEntryRepository.removeAllEntries();
                 $scope.update();
+            };
+
+            $scope.exportSummary = function () {
+                var data = '';
+
+                for (var i = 0; i < $scope.timeTrackerSummaryEntries.length; i++) {
+                    var timeTrackerSummaryEntry = $scope.timeTrackerSummaryEntries[i];
+
+                    data += timeTrackerSummaryEntry.project;
+                    if (timeTrackerSummaryEntry.description) {
+                        data += ': ' + timeTrackerSummaryEntry.description;
+                    }
+                    data += ' (' + timeTrackerSummaryEntry.duration.format('h[h] m[m]') + ')';
+                    data += '\n';
+                }
+
+                var dataBase64 = $window.btoa(data);
+                var dataUrl = 'data:text/plain;charset="utf-8";base64,' + dataBase64;
+                $window.open(dataUrl);
             };
 
             $scope.calculate = function () {
